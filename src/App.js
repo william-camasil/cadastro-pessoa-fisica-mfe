977606d35594d98d869045d624d1effa6302132b
willliamcamasil <williamcamargosilva@gmail.com>
2026-01-08 07:12:18 -0300

    feat: adiciona configurações iniciais do projeto

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="cad-pessoa-fisica">
+      <p className="cad-pessoa-fisica-p">Cadastro Pessoa Fisica - MFE</p>
+    </div>
+  );
+};
+
+export default App;
